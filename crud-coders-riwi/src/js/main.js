1a6edcc397aceab6a0d0f11743a5384f7a7298aa
Luis <luisoro009@gmail.com>
2025-06-25 12:02:49 -0500

    Change in main.js for connect the DB

--- crud-coders-riwi/src/js/main.js
+++ crud-coders-riwi/src/js/main.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
             .then(response => response.json())
             .then(data => {
                 alert(`Productos Cargados Con Exito`);
+                
                 data.forEach(product => {
                     const tr = document.createElement('tr');
 
