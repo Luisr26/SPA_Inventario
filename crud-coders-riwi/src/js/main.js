7ba405341ec6a27be2bed8c30f498829c28d8115
Luis <luisoro009@gmail.com>
2025-06-25 12:02:21 -0500

    Change in main.js for connect the DB

--- crud-coders-riwi/src/js/main.js
+++ crud-coders-riwi/src/js/main.js
@@ -1,51 +1,123 @@
 document.addEventListener('DOMContentLoaded', () => {
     const save = document.querySelector('#btn-save');
     const ID = document.querySelector('#id-product');
-    const name = document.querySelector('#name-product');
-    const quantity = document.querySelector('#quantity-product');
-    const price = document.querySelector('#price-product');
     const tbody = document.querySelector('.container_master');
+
+
+    window.onload = () => {
+        fetch('http://localhost:3000/inventario', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                alert(`Productos Cargados Con Exito`);
+                data.forEach(product => {
+                    const tr = document.createElement('tr');
+
+                    const tdId = document.createElement('td');
+                    tdId.textContent = product.id;
+                    tr.appendChild(tdId);
+
+                    const tdProducto = document.createElement('td');
+                    tdProducto.textContent = product.name;
+                    tr.appendChild(tdProducto);
+
+                    const tdCantidad = document.createElement('td');
+                    tdCantidad.textContent = product.quantity;
+                    tr.appendChild(tdCantidad);
+
+                    const tdPrecio = document.createElement('td');
+                    tdPrecio.textContent = product.price;
+                    tr.appendChild(tdPrecio);
+
+                    const tdOpciones = document.createElement('td');
+                    const divBtns = document.createElement('div');
+                    divBtns.className = 'td-btn';
+
+                    const btnUpdate = document.createElement('button');
+                    btnUpdate.textContent = 'Update';
+                    btnUpdate.id = 'update-product';
+
+                    const btnDelete = document.createElement('button');
+                    btnDelete.textContent = 'Delete';
+                    btnDelete.id = 'delete-product';
+
+                    divBtns.appendChild(btnUpdate);
+                    divBtns.appendChild(btnDelete);
+                    tdOpciones.appendChild(divBtns);
+                    tr.appendChild(tdOpciones);
+
+                    tbody.appendChild(tr);
+                });
+            })
+    }
+
 
     save.addEventListener("click", (e) => {
         e.preventDefault();
 
-        const tr = document.createElement('tr');
+        const name = document.querySelector('#name-product').value;
+        const quantity = document.querySelector('#quantity-product').value;
+        const price = document.querySelector('#price-product').value;
+        const id = ID.value;
 
-        const tdId = document.createElement('td');
-        tdId.textContent = ID.value;
-        tr.appendChild(tdId);
+        fetch('http://localhost:3000/inventario', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id,
+                name,
+                quantity,
+                price
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                alert(`Usuario guardado: ${JSON.stringify(data)}`);
+                const tr = document.createElement('tr');
 
-        const tdProducto = document.createElement('td');
-        tdProducto.textContent = name.value;
-        tr.appendChild(tdProducto);
+                const tdId = document.createElement('td');
+                tdId.textContent = id;
+                tr.appendChild(tdId);
 
-        const tdCantidad = document.createElement('td');
-        tdCantidad.textContent = quantity.value;
-        tr.appendChild(tdCantidad);
+                const tdProducto = document.createElement('td');
+                tdProducto.textContent = name;
+                tr.appendChild(tdProducto);
 
-        const tdPrecio = document.createElement('td');
-        tdPrecio.textContent = price.value;
-        tr.appendChild(tdPrecio);
+                const tdCantidad = document.createElement('td');
+                tdCantidad.textContent = quantity;
+                tr.appendChild(tdCantidad);
 
-        const tdOpciones = document.createElement('td');
+                const tdPrecio = document.createElement('td');
+                tdPrecio.textContent = price;
+                tr.appendChild(tdPrecio);
 
-        const divBtns = document.createElement('div');
-        divBtns.className = 'td-btn';
+                const tdOpciones = document.createElement('td');
+                const divBtns = document.createElement('div');
+                divBtns.className = 'td-btn';
 
-        const btnUpdate = document.createElement('button');
-        btnUpdate.textContent = 'Update';
-        btnUpdate.id = 'update-product';
+                const btnUpdate = document.createElement('button');
+                btnUpdate.textContent = 'Update';
+                btnUpdate.id = 'update-product';
 
-        const btnDelete = document.createElement('button');
-        btnDelete.textContent = 'Delete';
-        btnDelete.id = 'delete-product';
+                const btnDelete = document.createElement('button');
+                btnDelete.textContent = 'Delete';
+                btnDelete.id = 'delete-product';
 
-        divBtns.appendChild(btnUpdate);
-        divBtns.appendChild(btnDelete);
-        tdOpciones.appendChild(divBtns);
-        tr.appendChild(tdOpciones);
+                divBtns.appendChild(btnUpdate);
+                divBtns.appendChild(btnDelete);
+                tdOpciones.appendChild(divBtns);
+                tr.appendChild(tdOpciones);
 
-        document.querySelector('tbody').appendChild(tr);
-
+                tbody.appendChild(tr);
+            })
+            .catch(error => {
+                console.error(`Error al guardar: ${error}`);
+            });
     });
 });
